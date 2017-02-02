65f64690aee8f253b5f4fdcdc90ae521256c15e5
abhishekmantri <abhishekmantri268@gmail.com>
2017-02-03 01:59:35 +0530

    form validation applied

--- Online cv builder/js/main.js
+++ Online cv builder/js/main.js
@@ -161,6 +161,11 @@
 
 	});
 
+
+
+
+
+	
 	// Mailchimp translation
 	//
 	//  Defaults:
@@ -208,6 +213,17 @@
     		$.magnificPopup.close();
     });
 
+
+
+function validateForm() {
+    var x = document.forms["myForm"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+};
 
  	/*----------------------------------------------------- */
   	/* Back to top
